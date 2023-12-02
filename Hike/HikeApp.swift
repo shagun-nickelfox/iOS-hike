8016e158b9a7f1d584cfe6972172084402fdd6bc
shagun-nickelfox <shagun.bansal@nickelfox.com>
2023-12-02 18:06:45 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Nickelfox on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
